3061e5693338a776bd3e018bfd6a89e6c108946e
Marko Dumnic <markodumnic8@gmail.com>
2021-08-23 15:12:34 +0200

    Fixed icons for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     // SCSS file in the project
-    '@/assets/sass/main.scss'
+    '@/assets/sass/main.scss',
     '@fortawesome/fontawesome-free/css/all.css'
   ],
 
